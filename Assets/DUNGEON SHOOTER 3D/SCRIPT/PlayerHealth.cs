c8a23e312dfa99e8dcdd1c1ef0ab77b249dd6508
OdiljOVLIYEV <109040431+OdiljOVLIYEV@users.noreply.github.com>
2024-07-23 10:39:08 +0500

    23.07.2024

---
+++ Assets/DUNGEON SHOOTER 3D/SCRIPT/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Obvious.Soap;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public float health = 100f;
+    private void Start()
+    {
+        text.text=health.ToString();
+        GetComponent<Animator>().enabled = false;
+		
+    }
+
+    public void TakeDamage(float amount)
+    {	
+        health -= amount;
+        text.text=health.ToString();
+	
+        if (health <= 0f)
+        {
+            Die(); 
+			
+			
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<Animator>().enabled = true;
+        Debug.Log("Player Died");
+        // Playerning o'lish logikasini shu yerda amalga oshiring
+    }
+	
+	
+	
+}
