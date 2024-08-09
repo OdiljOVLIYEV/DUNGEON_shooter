9275713418f49fdaca030323306e9ec0c7389730
OdiljOVLIYEV <109040431+OdiljOVLIYEV@users.noreply.github.com>
2024-08-10 03:13:15 +0500

    213213

--- Assets/DUNGEON SHOOTER 3D/SCRIPT/Bullet.cs
+++ Assets/DUNGEON SHOOTER 3D/SCRIPT/Bullet.cs
@@ -12,36 +12,32 @@ public class Bullet : MonoBehaviour
     public int maxDamage = 100;
 
     public bool rc_l_for_radius;
-   // public GameObject EFFECT;
+
     private void OnTriggerEnter(Collider other)
     {
-
         if (rc_l_for_radius)
         {
             Explode();
-            
-            
+            Destroy(gameObject,0.3f); // O'qni yo'q qilish
         }
         else
         {
-            
             IDamageable damageable = other.GetComponent<IDamageable>();
 
             if (damageable != null)
-            {    
+            {
                 damageable.TakeDamage(damage, transform.position, -transform.forward);
                 particalsystem();
-                Destroy(gameObject,3f); // O'qni yo'q qilish
+              
             }
+            
         }
-        
     }
 
     void Explode()
     {
         // Portlash effektini yaratish
-         particalsystem();
-        
+        particalsystem();
 
         // Yaqin atrofdagi obyektlarga ta'sir qilish
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
@@ -61,29 +57,22 @@ public class Bullet : MonoBehaviour
                 float distance = Vector3.Distance(transform.position, nearbyObject.transform.position);
                 float damagePercentage = Mathf.Clamp01((explosionRadius - distance) / explosionRadius);
                 float damage = maxDamage * damagePercentage;
-                Vector3 hitPoint =
-                    nearbyObject.ClosestPoint(transform.position); // Hit point - portlash markaziga yaqin nuqta
+                Vector3 hitPoint = nearbyObject.ClosestPoint(transform.position); // Hit point - portlash markaziga yaqin nuqta
                 Vector3 hitNormal = (nearbyObject.transform.position - transform.position).normalized; // Hit normal
 
                 damageable.TakeDamage(damage, hitPoint, hitNormal);
             }
         }
-
     }
 
-   
     private void particalsystem()
     {
-        
         switch (particleType)
         {
-			
             case ParticleType.ExplosiveEffect:
-                // O'q izini devorga joylashtirish
-                Instantiate(explosionEffect, transform.position, transform.rotation);// Izni devor ob'ektiga biriktirish
+                // Portlash effektini yaratish
+                Instantiate(explosionEffect, transform.position, transform.rotation);
                 break;
-		
         }
-        
     }
 }
